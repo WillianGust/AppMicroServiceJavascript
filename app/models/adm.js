6e68319522cf147c3f729715dca41367cd5f5274
WillianGust <williandes.silva@yahoo.ca>
2023-04-23 14:32:09 -0400

    WIP: create model, added jasmine in package

---
+++ app/models/adm.js
@@ -0,0 +1,19 @@
+const Adm = { 
+  id: 1, 
+  nome: "Willian",
+  senha: "123456",
+  todos: () => [
+    {
+      id: 2, 
+      nome: "Silva",
+      senha: "123456789",
+    },
+    {
+      id: 3, 
+      nome: "Wsilva",
+      senha: "12345678910",
+    }
+  ]
+ }
+
+ module.exports = Adm;
